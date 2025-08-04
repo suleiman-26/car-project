c24cedf64a61bbe96fc19634802dac89b8e02951
suleiman-26 <026suleiman@gmail.com>
2025-08-04 10:18:22 +0300

    Initial commit – salman.py and others

---
+++ salman.py
@@ -0,0 +1,84 @@
+'''class animal:
+    def __init__(self,name,species):
+       self.name=name
+       self.species=species
+    def desc(self):
+        print(f"this is {self.species} named {self.name}")
+a=animal("sungura","zozo")
+a.desc()
+class rectangle:
+    def __init__(self,width,height):
+       self.width=width
+       self.height=height
+    def area(self):
+        print(f" The area is {self.width*self.height}")
+s=rectangle(5,6)
+s.area()
+
+class laptop:
+    def __init__(self,brand,model,price):
+        self.brand=brand
+        self.model=model
+        self.price=price
+    def _info(self):
+         print(f"{self.brand} {self.model} {self.price}")
+h=laptop("lenovo","intel","700000")
+h._info()
+
+class emplyee:
+    def __init__(self,name,position,salary):
+        self.name=name
+        self.position=position
+        self.salary=salary
+    def give_raise(self):
+         print(f"{self.name}")
+         print(f"{self.position}")
+         print(f"{self.salary}")
+kh=emplyee("khamis","lecture","3500000")
+kh.give_raise()
+
+class movie:
+    def __init__(self,title,rating):
+        self.title=title
+        self.rating= rating
+    def is_hit(self):
+         print(f"{self.title}")
+         print(f"{self.rating}")
+w=movie("mission imposible","no")
+w.is_hit()
+
+
+class account:
+    def __init__(self,balance):
+        self.balance=balance
+    def deposit(self):
+        print(f"{self.balance}")
+    def withdraw(self):
+        print(f"{self.balance}")
+a=account(8888888)
+a.deposit()
+a=account(4444444)
+a.withdraw()'''
+
+
+'''class kitonga:
+    x=5
+a=kitonga()
+print(a.x)'''
+
+class car:
+    def __init__(self,rangi,maringi):
+        self.rangi=rangi
+        self.maringi=maringi
+    def asas(self):
+         print(f"{self.rangi} {self.maringi}")
+z1=car("pink","manne")
+z1.asas()
+        
+        
+        
+        
+
+
+    
+        
